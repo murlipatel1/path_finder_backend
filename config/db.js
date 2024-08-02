cf0ac43911372bd6e5f9fb2564703d0e3a92967c
Murli Patel <patelmurli15@gmail.com>
2024-08-02 16:59:32 +0530

    added the logic and comment

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 
 // Connect to the database
 const db = mongoose.connect('mongodb://localhost:27017/pathfinder');
-
+// const db = mongoose.connect('mongodb+srv://murlipatel28:<password>@cluster0.6fbas09.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 module.exports = db;
