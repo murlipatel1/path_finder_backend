b3da3a131b21b3d3594bcfe56e031137b8963a63
Murli Patel <patelmurli15@gmail.com>
2024-08-02 16:48:48 +0530

    database changes 1.1

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./config/db');
-// const run = require('./config/dbatlas');
+
 const cors = require('cors');
+
 //Default port 3000
 PORT = 3000 || process.env.PORT;
 
-// run().catch(console.dir);
+
 
 db.then(() => {
     console.log('Connected to database');
